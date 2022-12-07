5c2e5934f02122366b0915fe484e818a22d78306
Vladimir <bigcorvet@gmail.com>
2022-12-07 15:02:01 +0200

    first commit

--- core/settings.py
+++ core/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "portfolio_django",
         "USER": "postgres", 
-        "PASSWORD": "1305",
+        "PASSWORD": "****",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
